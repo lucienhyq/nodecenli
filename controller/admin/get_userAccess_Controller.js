a76cc41eefc6c54dff94bde384cd96a30c3b5fca
SwagDad <47840560+lucienhyq@users.noreply.github.com>
2023-10-21 23:45:08 +0800

    update

--- controller/admin/get_userAccess_Controller.js
+++ controller/admin/get_userAccess_Controller.js
@@ -2,9 +2,8 @@ const UserAccessModel = require('../../models/admin/UserAccess');
 const logger = require('../../logs/logs').logger;
 
 const UserAccess = async (req, res, next) => {
-  logger.info(req.query, req.route.path, req.method)
-  logger.info(req.session)
   let list = await UserAccessModel.find();
+  logger.info(req.session,list)
   res.status(200).send({
     result: 1,
     msg: '已经登录',
