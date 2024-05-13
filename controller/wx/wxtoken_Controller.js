12b263d1fcec29700387642875af6bf1ecfa92a3
swagDad <970207605@qq.com>
2024-05-13 09:24:16 +0800

    update

--- controller/wx/wxtoken_Controller.js
+++ controller/wx/wxtoken_Controller.js
@@ -4,46 +4,49 @@ const fs = require("fs");
 
 const miniToken = async (req, res, next) => {
   let i_type = req.query.i || req.body.i;
-  console.log("miniToken", i_type)
+  console.log("miniToken", i_type);
+  // i=1是微信小程序
   if (req.body.i != 1) {
-    // i=1是微信小程序
-    next()
-    return
+    next();
+    return;
   }
   let dayTime = Date.parse(new Date()) / 1000;
   let PUBLIC_PATH = path.resolve(__dirname, "../../js/miniToken.json");
-  await fs.readFile(PUBLIC_PATH, (err, data) => {
-    if (err) throw err
+  await fs.readFile(PUBLIC_PATH, async (err, data) => {
+    if (err) throw err;
     let jsonTokenTime = JSON.parse(data.toString()).expires_in;
-    console.log(jsonTokenTime, dayTime, jsonTokenTime)
+    console.log(jsonTokenTime, dayTime, jsonTokenTime);
     if (!jsonTokenTime || dayTime > jsonTokenTime) {
       // 凭证过期
-      requestGet(
+      await requestGet(
         `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxe6d74c44b857ba7b&secret=00c2105ad289a858de30209a10b73295`
-      ).then((data) => {
+      ).then(async (data) => {
         let accessToken = JSON.parse(data);
         // console.log(accessToken.expires_in,dayTime)
-        accessToken.expires_in = Number(dayTime) + Number(accessToken.expires_in);
+        accessToken.expires_in =
+          Number(dayTime) + Number(accessToken.expires_in);
         // console.log(accessToken.expires_in)
-        fs.writeFile(PUBLIC_PATH, JSON.stringify(accessToken), function (err) {
-          if (err) {
-            console.log(err);
+        await fs.writeFile(
+          PUBLIC_PATH,
+          JSON.stringify(accessToken),
+          function (err) {
+            if (err) {
+              console.log(err);
+            }
           }
-        });
+        );
         next();
       });
     } else {
       // 没过期
-      console.log('wxtoken_Controller凭证没有过期')
+      console.log("wxtoken_Controller凭证没有过期");
       // res.json({
       //   data: JSON.parse(data.toString())
       // })
-      req.query.jsonTokenTime = JSON.parse(data.toString())
+      req.query.jsonTokenTime = JSON.parse(data.toString());
       next();
     }
-  })
-
-
+  });
 };
 module.exports = miniToken;
 
